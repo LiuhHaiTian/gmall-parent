92454a90be9a1f5b44d9d9372cdda1f40c209dec
Liuhaitian <1510552424@qq.com>
2019-11-26 17:00:03 +0800

    Initial commit

---
+++ gmall0624-logger/src/main/java/com/atguigu/gmall0624/logger/controller/LoggerController.java
@@ -0,0 +1,26 @@
+package com.atguigu.gmall0624.logger.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Slf4j
+public class LoggerController {
+
+    @PostMapping("log")
+    public String log(@RequestParam("logString") String logString){
+        System.out.println(logString);
+            //补时间戳
+        JSONObject jsonObject = JSON.parseObject(logString);
+        jsonObject.put("ts",System.currentTimeMillis());
+        
+        //落盘日志文件
+        String jsonString = jsonObject.toJSONString();
+        log.info(jsonString);
+            //发送kafka
+
+        return "ok";
+    }
+}
